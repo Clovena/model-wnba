6a65a48252aeb9d6a2c2a3f47ddfd09c108855e1
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2026-05-26 08:29:34 -0400

    Init py files and explo of raw data.

--- ingestion/explore.py
+++ ingestion/explore.py
@@ -2,7 +2,7 @@ import json
 from extract.espn_client import get
 
 # What does a single day's scoreboard look like?
-data = get("scoreboard", params={"dates": "20250509"})
+data = get("scoreboard", params={"dates": "20250532"})
 
 # Pretty print so we can see the full structure
 print(json.dumps(data, indent=2))
