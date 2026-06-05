e8a36399725a8b20c5c5597896afea0b2da245eb
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2026-06-05 16:12:37 -0400

    Refactor raw py load to break out broadcast and competitor data separately from competitions.

--- ingestion/load/raw_load.py
+++ ingestion/load/raw_load.py
@@ -36,41 +36,74 @@ for s in seasons:
 metadata_df = []
 links_df = []
 competitions_df = []
+broadcasts_df = []
+competitors_df = []
 
 for d in valid_dates:
   print(d)
   raw_data = get('scoreboard', params={'dates': d})
   events = raw_data['events']
   df = pd.DataFrame(events)
-
   # Normalize simple metadata
   id_df = df[['id', 'uid', 'date', 'name', 'shortName']]
   season_df = pd.json_normalize(df.season)
   status_df = pd.json_normalize(df.status)
   metadata_temp = id_df.join(season_df).join(status_df)
-
   # Normalize links array
-  links_temp = pd.json_normalize(df.links.iloc[0])
-  links_temp['game_id'] = df['id'].iloc[0]
-
+  links_list = []
+  for _, row in df.iterrows():
+      game_links = pd.json_normalize(row['links'])
+      game_links['game_id'] = row['id']
+      links_list.append(game_links)
+  links_temp = pd.concat(links_list, ignore_index=True)
   # Normalize competitions array
-  competitions_temp = pd.json_normalize(df.competitions.iloc[0]).rename(columns={'id': 'game_id'})
-
+  competitions_list = []
+  for _, row in df.iterrows():
+      comp = pd.json_normalize(row['competitions'][0])
+      comp = comp.rename(columns={'id': 'game_id'})
+      competitions_list.append(comp)
+  competitions_temp = pd.concat(competitions_list, ignore_index=True)
+  ## Sub-arrays from competitions
+  broadcasts_list = []
+  competitors_list = []
+  for _, row in competitions_temp.iterrows():
+      game_id = row['game_id']
+      comps = pd.json_normalize(row['competitors'])
+      comps['game_id'] = game_id
+      competitors_list.append(comps)
+      for broadcast in row['broadcasts']:
+          broadcast['game_id'] = game_id
+          broadcasts_list.append(broadcast)
+  broadcasts_temp = pd.DataFrame(broadcasts_list)
+  competitors_temp = pd.concat(competitors_list, ignore_index=True)
+  competitors_temp = competitors_temp.drop(
+    columns=['broadcasts', 'competitors','linescores', 'statistics', 'leaders', 'records', 'team.links'],
+    errors='ignore'
+  )
+  # Append dfs
   metadata_df.append(metadata_temp)
   links_df.append(links_temp)
   competitions_df.append(competitions_temp)
+  broadcasts_df.append(broadcasts_temp)
+  competitors_df.append(competitors_temp)
 
 # Load to DuckDB
-metadata_df = pd.concat(metadata_df, ignore_index=True)
-links_df = pd.concat(links_df, ignore_index=True)
-competitions_df = pd.concat(competitions_df, ignore_index=True)
+metadata_df = pd.concat(metadata_df, ignore_index = True)
+links_df = pd.concat(links_df, ignore_index = True)
+competitions_df = pd.concat(competitions_df, ignore_index = True)
+broadcasts_df = pd.concat(broadcasts_df, ignore_index = True)
+competitors_df = pd.concat(competitors_df, ignore_index = True)
 
 con.execute("CREATE SCHEMA IF NOT EXISTS raw")
 
 con.register('metadata_df', metadata_df)
 con.register('links_df', links_df)
 con.register('competitions_df', competitions_df)
+con.register('broadcasts_df', broadcasts_df)
+con.register('competitors_df', competitors_df)
 
 con.execute("CREATE OR REPLACE TABLE raw.games AS SELECT * FROM metadata_df")
 con.execute("CREATE OR REPLACE TABLE raw.game_links AS SELECT * FROM links_df")
 con.execute("CREATE OR REPLACE TABLE raw.competitions AS SELECT * FROM competitions_df")
+con.execute("CREATE OR REPLACE TABLE raw.broadcasts AS SELECT * FROM broadcasts_df")
+con.execute("CREATE OR REPLACE TABLE raw.competitors AS SELECT * FROM competitors_df")
